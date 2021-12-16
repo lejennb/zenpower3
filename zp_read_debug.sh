3773f2a7b2e905279c607e3d5dbe197c57b8755f
Anthony Wang <ta180m@pm.me>
2021-12-16 11:24:40 -0600

    Create debugging script
    https://gist.github.com/ocerman/6566b00072c8af96a109f65075f2d8b7

---
+++ zp_read_debug.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+hwmon="/sys/class/hwmon"
+mdevs=`ls $hwmon`
+zenmon=""
+ok=0
+
+for dev in $mdevs; do
+    path="$hwmon/$dev"
+    devname=`cat $path/name`
+    if [ "$devname" == "zenpower" ]; then
+        cat $path/debug_data
+        ok=1
+    fi
+done
+
+if [ $ok -ne 1 ]; then
+    echo "Zenpower not found"
+    exit
+fi
